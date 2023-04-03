c3bf27c1b00a71974fa4b77bc8c46cf6513eeaba
NatasaMiric <natasa.miric94@gmail.com>
2023-04-03 10:59:41 +0000

    Create Followers model

--- likes/views.py
+++ likes/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
-from likes.models import Like
-from likes.serializers import LikeSerializer
+from likes.models import Likes
+from likes.serializers import LikesSerializer
 
 
 class LikeList(generics.ListCreateAPIView):
@@ -9,8 +9,8 @@ class LikeList(generics.ListCreateAPIView):
     List likes or create a like if logged in.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -21,5 +21,5 @@ class LikeDetail(generics.RetrieveDestroyAPIView):
     Retrieve a like or delete it by id if you own it.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
